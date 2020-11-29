6ed444bd4f0e374b44aa56c703ea5ba2fb662310
guillecipri <guillecipriani@gmail.com>
2020-11-29 19:02:59 -0300

    completando app usuarios

--- optometria/app_usuarios/urls.py
+++ optometria/app_usuarios/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    path("usuarios/", views.usuarios, name="usuarios")
+    path("usuarios", views.usuarios, name="usuarios"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
